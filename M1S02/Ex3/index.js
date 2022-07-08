99718e730f72ca0fee733af70473f4ab0d0216ca
Guigghisi <guilhermeghisi14@gmail.com>
2022-07-08 20:13:21 -0300

    M1S02E3

---
+++ M1S02/Ex3/index.js
@@ -0,0 +1,7 @@
+var primeiroNumero = prompt("Digite o primeiro número");
+var segundoNumero = prompt("Digite o segundo número");
+document.write(
+  "<h4>A soma dos números é: " +
+    (parseInt(primeiroNumero) + parseInt(segundoNumero)) +
+    "</h4>"
+);
