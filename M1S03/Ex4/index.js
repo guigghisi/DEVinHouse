e58970961be66f016546dd69c0cb0c0ceee81ab9
Guigghisi <guilhermeghisi14@gmail.com>
2022-07-15 20:22:56 -0300

    M1S03E4

---
+++ M1S03/Ex4/index.js
@@ -0,0 +1,13 @@
+const divPrincipal = document.getElementsByTagName("div")[0];
+const criarDiv1 = document.createElement("div");
+criarDiv1.innerHTML += "<p>Texto qualquer </p>";
+divPrincipal.appendChild(criarDiv1);
+const criarDiv2 = document.createElement("div");
+criarDiv2.innerHTML += "<p>Texto qualquer </p>";
+divPrincipal.appendChild(criarDiv2);
+const criarDiv3 = document.createElement("div");
+criarDiv3.innerHTML += "<p>Texto qualquer </p>";
+divPrincipal.appendChild(criarDiv3);
+const criarDiv4 = document.createElement("div");
+criarDiv4.innerHTML += "<p>Texto qualquer </p>";
+divPrincipal.appendChild(criarDiv4);
