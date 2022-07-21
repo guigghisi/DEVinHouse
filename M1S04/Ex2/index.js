add42e3cb63bc59c2cccede82e5d94334b8b1a03
Guigghisi <guilhermeghisi14@gmail.com>
2022-07-21 13:40:19 -0300

    M1S04E2

---
+++ M1S04/Ex2/index.js
@@ -0,0 +1,10 @@
+class Filme {
+  nome;
+  anoDeLancamento;
+  diretor;
+};
+
+const filme1 = new Filme();
+filme1.nome = "Kung Fu Panda 3";
+filme1.anoDeLancamento = 2016;
+filme1.diretor = "Jennifer Yuh Nelson e Alessandro Carloni";
