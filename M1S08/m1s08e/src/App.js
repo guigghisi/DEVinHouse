b0135aa4c43c951dd4e428fb279d902374b25426
Guigghisi <guilhermeghisi14@gmail.com>
2022-08-19 21:02:38 -0300

    M1S08E5

--- M1S08/m1s08e/src/App.js
+++ M1S08/m1s08e/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Titulo from "./Componentes/Titulo";
 import Texto from "./Componentes/Texto";
+import Soma from "./Componentes/Soma";
 function App() {
   return (
     <div className="App">
       <Titulo />
       <Texto />
+      <Soma numero1={10} numero2={20} />
     </div>
   );
 }
---
+++ M1S08/m1s08e/src/Componentes/Soma.js
@@ -0,0 +1,5 @@
+const Soma = ({numero1, numero2}) => {
+    
+  return <p>A soma dos números é {numero1 + numero2}</p>;
+};
+export default Soma;
