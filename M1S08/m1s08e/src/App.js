e1b53a79a9649321ba375b2791135865f47352ec
Guigghisi <guilhermeghisi14@gmail.com>
2022-08-23 14:54:08 -0300

    M1S08E10

--- M1S08/m1s08e/src/App.js
+++ M1S08/m1s08e/src/App.js
@@ -1,29 +1,14 @@
 import "./App.css";
-import PropTypes from "prop-types";
-import Produto from "./Componentes/Produto/Produto";
+import Condicional from "./Componentes/Condicional";
 
 export default function App({ isLoading }) {
   if (isLoading === true) {
     return <p>Carregando...</p>;
   }
-  const produtos = [
-    {
-      imagem:
-        "https://http2.mlstatic.com/D_NQ_NP_632159-MLB50098814188_052022-O.webp",
-      nome: "Produto 1",
-      preco: "R$ 100,00",
-    },
-  ];
-  Produto.propTypes = {
-    nome: PropTypes.string.isRequired,
-    imagem: PropTypes.string.isRequired,
-    preco: PropTypes.string.isRequired,
-  };
+
   return (
     <>
-      {produtos.map((produto) => (
-        <Produto key={produto.nome} {...produto} />
-      ))}
+      <Condicional condicao={true} />
     </>
   );
 }
---
+++ M1S08/m1s08e/src/Componentes/Condicional.js
@@ -0,0 +1,4 @@
+export default function Condicional({ condicao }) {
+  return <>{condicao ? <p>Condição verdadeira</p> : <p>Condição falsa</p>}</>;
+}
+
